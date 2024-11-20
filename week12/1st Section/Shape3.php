ea06d58530ef580bea62355e87ad43db99cdb46f
they call me Arab <144300459+hemoabdullah@users.noreply.github.com>
2024-11-20 12:42:35 +0700

    Add files via upload

---
+++ week12/1st Section/Shape3.php
@@ -0,0 +1,36 @@
+<?php
+
+interface Shape3
+{
+    public function calculateArea();
+}
+
+interface Color
+{
+    public function getColor();
+}
+
+class Circle implements Shape3, Color
+{
+    private $radius;
+    private $color;
+
+    public function __construct($radius, $color)
+    {
+        $this->radius = $radius;
+        $this->color = $color;
+    }
+
+    public function calculateArea()
+    {
+        return pi() * pow($this->radius, 2);
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+}
+$circle = new Circle(5, "Blue");
+echo "Area of Circle: " . $circle->calculateArea() . "<br>";
+echo "Color of Circle: " . $circle->getColor() . "<br>";
